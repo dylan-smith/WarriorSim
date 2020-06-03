e744c707f2acc2a924506306220758fdb87595e2
Dylan Smith <dylanfromwinnipeg@gmail.com>
2020-06-02 23:01:33 -0500

    Created actions workflow to build and deploy

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,47 @@ var cssnano = require('gulp-cssnano');
 var sass = require('gulp-sass');
 var minify = require('gulp-minify');
 var browser = require('browser-sync').create();
+var inject = require('gulp-inject');
+var cacheBust = require('gulp-cache-bust');
+var concat = require('gulp-concat');
 
 gulp.task('js', function() {
-    gulp.src(['js/**/*.js', 'lib/*.mjs'])
-      .pipe(minify({
-        noSource: true,
-        ext:{
-            min:'.min.js'
-        },
-      }))
-      .pipe(gulp.dest('dist/js'))
-  });
+    return gulp.src('js/**/*.js')
+               .pipe(concat('warriorsim.js'))
+               .pipe(minify({
+                        noSource: true,
+                        ext: {
+                            min:'.min.js'
+                        },
+               }))
+               .pipe(gulp.dest('dist/js'))
+});
 
 gulp.task('sass', function(){
    return gulp.src('scss/style.scss')
-      .pipe(sass())
-      .pipe(cssnano())
-      .pipe(gulp.dest('dist/css'));
+              .pipe(sass())
+              .pipe(cssnano())
+              .pipe(gulp.dest('dist/css'));
 });
+
+gulp.task('img', function() {
+    return gulp.src('img/**')
+               .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('libs', function() {
+    return gulp.src(['node_modules/jquery/dist/jquery.min.js',
+                     'node_modules/tablesorter/dist/js/jquery.tablesorter.min.js', 
+                     'node_modules/tablesorter/dist/js/jquery.tablesorter.widgets.min.js',
+                     'node_modules/chart.js/dist/Chart.min.js'])
+               .pipe(concat('libs.min.js'))
+               .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('theme', function() {
+    return gulp.src('node_modules/tablesorter/dist/css/theme.default.min.css')
+               .pipe(gulp.dest('dist/css'));
+})
 
 gulp.task('watch', function(){
     gulp.watch('scss/*.scss', ['sass']);
@@ -35,4 +58,16 @@ gulp.task('browser', function() {
     });
 });
 
-gulp.task('default', ['sass', 'js', 'watch', 'browser']);
+gulp.task("html", ["sass", "js", "libs", "theme"], function () {
+    let sources = gulp.src(["./dist/**/*.js", "./dist/css/style.css"], {read: false});
+  
+    return gulp.src("index.html")
+               .pipe(inject(sources, { ignorePath: 'dist/', addRootSlash: false }))
+               .pipe(inject(gulp.src('dist/css/theme.default.min.css'), { name: 'inject-theme', ignorePath: 'dist/', addRootSlash: false }))
+               .pipe(cacheBust({ basePath: 'dist/' }))
+               .pipe(gulp.dest("./dist"));
+  });
+  
+gulp.task("build", ["sass", "js", "img", "libs", "theme", "html"]);
+
+gulp.task('default', ['build', 'watch', 'browser']);
